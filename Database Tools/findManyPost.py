34880a9d4ccb1c954dba5e6018b0132474c858d4
James Lynch <lynchj1@ufl.edu>
2024-07-06 13:18:59 -0400

    Added search functions

---
+++ Database Tools/findManyPost.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+
+
+from databasekeys import cluster
+
+# db and collection are the parameters used to get to desired section of database
+# parameter and value filter which posts you are searching for
+# Returns empty vector if no results are found
+
+def findManyPost(db, collection, parameter, value):
+    database = cluster[db]
+    coll = database[collection]
+    try:
+        results = coll.find({parameter: value})
+        print("Search success.")
+        data = []
+        for result in results:
+            data.append(result)
+        return data
+    except:
+        print("Search failed.")
+
+
+print(findManyPost("Inventory", "Books", "genre", "Fanasy"))
