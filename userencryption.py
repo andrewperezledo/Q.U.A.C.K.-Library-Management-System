2ef41af5146a2a4e0e91ffdaeed1a16587346c09
Don <robinson.d@ufl.edu>
2024-07-08 20:17:00 -0400

    Condensed databasetools folder. Actual functions written by Jimmy.

---
+++ userencryption.py
@@ -0,0 +1,19 @@
+from cryptography.fernet import Fernet
+from databasekeys import encryptionKey
+
+# password is the basic text password you want to encrypt
+# it uses the cryptography package
+def passwordEncrypt(password):
+
+    fernet = Fernet(encryptionKey)
+
+    return fernet.encrypt(password.encode())
+
+
+# password needs to be the fernet.encrypted password, should be a jumble of text
+#  it uses the cryptography package
+def passwordDecrypt(encryptedPassword):
+
+    fernet = Fernet(encryptionKey)
+
+    return fernet.decrypt(encryptedPassword).decode()
