527e2efbb7ebd7830d4a331bc57ed20dd9ea6a21
James Lynch <lynchj1@ufl.edu>
2024-07-11 15:52:05 -0400

    Login page fully functioning

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, session, flash
-from DatabaseTools.databasetools import userCreation 
+from DatabaseTools.databasetools import userCreation
+from DatabaseTools.databasetools import loginFunction
+from DatabaseTools.databasetools import userSearch
 # to run, export this file with export FLASK_APP=home, export FLASK_DEBUG=1
 # to run mutiple apps, use -p like this: flask run -p 5001 *******to change port 
 # templates for the html & js inside html, static for CSS, JavaScript, & images
@@ -17,28 +19,29 @@ def homepage():
 @app.route('/create-user', methods=('GET', 'POST'))
 
 def create_user():
-    if request.method == 'POST':
-        name = request.form.get('content')
-        student = request.form.get('degree')
-        # SEARCH IF USER ALREADY EXISTS!!!!!
+    if session["usertype"] == "employee" or session["usertype"] == "admin": # This line only lets admin or employees use this page
+        if request.method == 'POST':
+            name = request.form.get('content')
+            student = request.form.get('degree')
+            # SEARCH IF USER ALREADY EXISTS!!!!!
 
-        # Process the form data (e.g., save to database)
-        #create = userCreation(name, "password123", "member")
-        session['username'] = name
-        session['usertype'] = "member"
-        # if(create == "Matching Username"):
-        #     # User already exists
-        #     # implement something to tell user they already exist
-        #     return render_template('create_user.html')
-        # elif(create == "success"):
-        #     # User is created and sent to user home page
-        #     session['username'] = name
-        #     #session['usertype'] = "member"
-        #     return redirect(url_for('home_user', username=name))
+            # Process the form data (e.g., save to database)
+            #create = userCreation(name, "password123", "member")
+            session['username'] = name
+            session['usertype'] = "member"
+            # if(create == "Matching Username"):
+            #     # User already exists
+            #     # implement something to tell user they already exist
+            #     return render_template('create_user.html')
+            # elif(create == "success"):
+            #     # User is created and sent to user home page
+            #     session['username'] = name
+            #     #session['usertype'] = "member"
+            #     return redirect(url_for('home_user', username=name))
 
-        # Redirect to the homepage route
-        return redirect(url_for('home_user', username=name))
-    return render_template('create_user.html')
+            # Redirect to the homepage route
+            return redirect(url_for('home_user', username=name))
+        return render_template('create_user.html')
 
 @app.route('/login', methods=('GET', 'POST'))
 
@@ -46,8 +49,15 @@ def login_page():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        create = userCreation(username, password,"member")
-        return redirect(url_for('home_user', username=username))
+        result = loginFunction(username, password)
+        if result == "Login Successful.":
+            userdata = userSearch(username)
+            session['username'] = username
+            session['usertype'] = userdata["usertype"]  # Admin, employee, or member
+            return redirect(url_for('home_user', username=username))
+        else:
+            flash("Invalid username or password", "info")
+            return redirect(url_for('login_page'))
     return render_template('login_page.html')
 
 
@@ -59,11 +69,13 @@ def home_user():
         username = request.args.get('username')
         if session["usertype"] == "member":
             return render_template('test_memberonly.html')
+        elif session["usertype"] == "employee":
+            return render_template('test_employeeonly.html')
         elif session["usertype"] == "admin":
             return render_template('test_adminonly.html')
-        return render_template('home_user.html', username = username)
+        #return render_template('home_user.html', username = username)
     else:
-        return redirect(url_for('create_user'))
+        return redirect(url_for('login_page'))
 
 @app.route("/catalog")
 def catalog():
