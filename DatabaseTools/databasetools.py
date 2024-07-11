a6db361004d783bb1629d4e434b4a8ea7d92082e
Andrew Perez-Ledo <andrewperezledo@gmail.com>
2024-07-11 13:37:26 -0400

    updating to main, login page and calendar button

---
+++ DatabaseTools/databasetools.py
@@ -0,0 +1,228 @@
+import pymongo
+from bson.json_util import dumps
+from pymongo import MongoClient
+
+from DatabaseTools.databasekeys import cluster
+from DatabaseTools.userencryption import passwordDecrypt, passwordEncrypt
+
+
+# db and collection are the parameters used to get to desired section of database
+# posts are the new items you are adding to the database
+# Duplicate _id items fail to add
+def addManyPost(db, collection, posts):
+    database = cluster[db]
+    coll = database[collection]
+    try:
+        result = coll.insert_many(posts, ordered=False)
+        print("Posts created.")
+    except pymongo.errors.BulkWriteError as e:
+        print(e.details['writeErrors'][0]['errmsg'])
+# Example:
+# post1 = {"_id": "978-0590353427", "title": "Harry Potter and the Sorcerer's Stone", "genre": "Fantasy",
+#      "rec_grade": "Middle"}
+# post2 = {"_id": "978-0439064873", "title": "Harry Potter and the Chamber of Secrets", "genre": "Fantasy",
+#         "rec_grade": "Middle"}
+# post3 = {"_id": "978-0439136365", "title": "Harry Potter and the Prisoner of Azkaban", "genre": "Fantasy",
+#         "rec_grade": "Middle"}
+# post4 = {"_id": "978-0439139601", "title": "Harry Potter and the Goblet of Fire", "genre": "Fantasy",
+#         "rec_grade": "Middle"}
+# post5 = {"_id": "978-0439358071", "title": "Harry Potter and the Order of the Phoenix", "genre": "Fantasy",
+#         "rec_grade": "Middle"}
+
+# addManyPost("Inventory", "Books", [post1, post2, post3, post4, post5])
+
+
+# Try except borrowed:
+# https://stackoverflow.com/questions/44838280/how-to-ignore-duplicate-key-errors-safely-using-insert-many
+
+# db and collection are the parameters used to get to desired section of database
+# post is the new item you are adding to the database
+# Duplicate _id items fail to add
+def addPost(db, collection, post):
+    database = cluster[db]
+    coll = database[collection]
+    try:
+        coll.insert_one(post)
+        return "success"
+    except pymongo.errors.DuplicateKeyError as e:
+        return "Duplicate Key"
+    except:
+        return "fail"
+
+
+# enter title of book to check out, username is the user who is checking out book
+# checks if book exists
+# does not have user permission validation, so at this point any user can do this
+def bookCheckout(title, username):
+
+    if not checkBookAvailability(title):
+        return "Book unavailable"
+
+    data = findPost("Inventory", "Books", "title", title)
+    if data is None:
+        return "Title does not exist"
+
+    # Sets book to unavailable in inventory system
+    updatePost("Inventory", "Books", "title", title, "available", False)
+    # Adds book to users profile
+    user = userSearch(username)
+    if len(user["inventory"]) == 0:
+        updatePost("Userdata", "Users", "_id", username, "inventory", [data])
+    else:
+        inv = user["inventory"]
+        inv.append(data)
+        updatePost("Userdata", "Users", "_id", username, "inventory", inv)
+
+    return "Book checked out"
+# Example:
+# print(bookCheckout("Harry Potter and the Sorcerer's Stone", "jimmylynch"))
+
+
+# function is designed to check item availability by title
+def checkBookAvailability(title):
+
+    data = findPost("Inventory", "Books", "title", title)
+
+    return data["available"]
+# Example:
+# checkBookAvailability("Harry Potter and the Order of the Phoenix")
+
+
+# db and collection are the parameters used to get to desired section of database
+# parameter and value filter which posts you will be deleting
+def deleteManyPost(db, collection, parameter, value):
+    database = cluster[db]
+    coll = database[collection]
+    try:
+        coll.delete_many({parameter: value})
+        print("Posts deleted.")
+    except:
+        print("Deletion failed.")
+# Example:
+# deleteManyPost("Inventory", "Books", "rec_grade", "Middle")
+
+
+# db and collection are the parameters used to get to desired section of database
+# parameter and value filter which post you will be deleting
+def deletePost(db, collection, parameter, value):
+    database = cluster[db]
+    coll = database[collection]
+    try:
+        coll.delete_one({parameter: value})
+        print("Post deleted.")
+    except:
+        print("Deletion failed.")
+# Example:
+# deletePost("Inventory", "Books", "_id","978-0590353427")
+
+
+# db and collection are the parameters used to get to desired section of database
+# parameter and value filter which posts you are searching for
+# Returns empty vector if no results are found
+def findManyPost(db, collection, parameter, value):
+    database = cluster[db]
+    coll = database[collection]
+    try:
+        results = coll.find({parameter: value})
+        print("Search success.")
+        data = []
+        for result in results:
+            data.append(result)
+        return data
+    except:
+        print("Search failed.")
+# Example:
+# print(findManyPost("Inventory", "Books", "genre", "Fanasy"))
+
+
+# db and collection are the parameters used to get to desired section of database
+# parameter and value filter which post you are trying to find
+# Duplicate _id items fail to add
+def findPost(db, collection, parameter, value):
+    database = cluster[db]
+    coll = database[collection]
+    try:
+        results = coll.find_one({parameter: value})
+        return results
+    except:
+        return "fail"
+# Example:
+# data = findPost("Userdata", "Users","_id","jimmylynch")
+# for items in data:
+#     if items == "password":
+#         print(passwordDecrypt(data[items]))
+#     else:
+#         print(data[items])
+
+
+# db and collection are the parameters used to get to desired section of database
+# search_parameter and search_value filter which posts you will be updating
+# new_parameter and new_value are what you are updating
+def updateManyPost(db, collection, search_parameter, search_value, new_parameter, new_value):
+    database = cluster[db]
+    coll = database[collection]
+    try:
+        result = coll.update_many({search_parameter: search_value},{"$set": {new_parameter: new_value}})
+        print("Posts updated.")
+    except:
+        print("Update failed.")
+# Example:
+# updateManyPost("Inventory","Books","genre", "Fantasy","available", True)
+
+
+# db and collection are the parameters used to get to desired section of database
+# search_parameter and search_value filter which post you will be updating
+# new_parameter and new_value are what you are updating
+# if there are multiple posts that match the search, only the first post found will update
+def updatePost(db, collection, search_parameter, search_value, new_parameter, new_value):
+    database = cluster[db]
+    coll = database[collection]
+    try:
+        coll.update_one({search_parameter: search_value}, {"$set":{new_parameter: new_value}})
+        print("Post updated.")
+    except:
+        print("Update failed.")
+# Example:
+# updatePost("Inventory", "Books", "title", "Harry Potter and the Sorcerer's Stone", "available", True)
+# updatePost("Userdata", "Users", "_id", "jimmylynch", "inventory", [])
+# updatePost("Inventory","Books","title","Harry Potter and the Sorcerer's Stone","genre","Magic")
+
+
+# username is the username, must be unique
+# password is password, will get encrypted
+# usertype should be either member, employee, or admin and will be used to enable account features
+def userCreation(username, password, usertype):
+    post = {"_id": username, "password": passwordEncrypt(password), "usertype": usertype}
+    add = addPost("Userdata", "Users", post)
+    if add == "Duplicate Key":
+        return "Matching Username"
+    else:
+        return add
+# Example:
+# print(userCreation("jimmylynch","badpassword","member"))
+
+
+# enter username
+# function returns all user information
+def userSearch(username):
+    return findPost("Userdata", "Users", "_id", username)
+# Example:
+# userSearch("jimmylynch")
+
+
+# enter username and password
+# function decrypts password and checks if user exists
+# returns either login successful or message stating incorrect parameters
+def loginFunction(username, password):
+
+    results = findPost("Userdata", "Users", "_id", username)
+    if results is None:
+        return "Username or Password is incorrect."
+
+    decpassword = passwordDecrypt(results["password"])
+    if (results["_id"] == username) and (password == decpassword):
+        return "Login Successful."
+    else:
+        return "Username or Password is incorrect."
+# Example:
+# print(loginFunction("jimmylynch","badpassword"))
--- app.py
+++ app.py
@@ -1,48 +1,3 @@
-<<<<<<< Updated upstream
-from flask import Flask, render_template, request, url_for, redirect
-
-# to run, export this file with export FLASK_APP=home, export FLASK_DEBUG=1
-# to run mutiple apps, use -p like this: flask run -p 5001 *******to change port 
-# templates for the html & js inside html, static for CSS, JavaScript, & images
-
-app = Flask(__name__)
-
-
-
-
-@app.route('/', methods=('GET', 'POST'))
-
-def homepage():
-
-    return render_template('homepage.html')
-
-
-@app.route('/create-user', methods=('GET', 'POST'))
-
-def create_user():
-    if request.method == 'POST':
-        name = request.form.get('content')
-        student = request.form.get('degree')
-        # search if user exists
-        # Process the form data (e.g., save to database)
-        
-        # Redirect to the homepage route
-        
-        return redirect(url_for('home_user', username=name))
-    return render_template('create_user.html')
-
-
-@app.route('/home-user', methods = ('GET', "POST"))
-
-def home_user():
-    username = request.args.get('username')
-    return render_template('home_user.html', username = username)
-
-
-if __name__ == '__main__': # DEVELOPMENT DEBUG MODE
-    app.debug = 1
-
-=======
 from flask import Flask, render_template, request, url_for, redirect
 from DatabaseTools.databasetools import userCreation 
 # to run, export this file with export FLASK_APP=home, export FLASK_DEBUG=1
@@ -103,4 +58,3 @@ def catalog():
 if __name__ == '__main__': # DEVELOPMENT DEBUG MODE
     app.run(debug=True)
 
->>>>>>> Stashed changes
